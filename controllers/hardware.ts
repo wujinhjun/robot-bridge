84aaa0326c0987391b5eacdf0cc2ff5d25ae38bf
wujinhjun <wujinhjun@outlook.com>
2023-11-15 12:15:44 +0000

    feat: hardware router

--- controllers/hardware.ts
+++ controllers/hardware.ts
@@ -4,7 +4,7 @@ import { SerialPort } from 'serialport';
 const hardwareRouter = express.Router();
 const portName = 'COM3';
 
-const serialPort = new SerialPort({ path: portName, baudRate: 9600 });
+
 
 hardwareRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
   console.log(req);
@@ -12,11 +12,26 @@ hardwareRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('hello');
 });
 
+// 返回串口列表
+hardwareRouter.get("/serials", async (req: Request, res: Response, next: NextFunction) =>{
+    const list = await SerialPort.list();
+    console.log(list);
+    res.json(list)
+})
+
+// 设置目标串口
+
+
+// 辅助函数需要能够判断当前串口是否正确
+
+// 如果不正确，则需要返回错误信息，正确则发送校验信息
+
 hardwareRouter.post(
   '/led',
   async (req: Request, res: Response, next: NextFunction) => {
+    const serialPort = new SerialPort({ path: portName, baudRate: 9600 });
     const data = req.body;
-    const list = await SerialPort.list();
+    
     console.log(data);
     serialPort.write(`${data.data ? 'a' : 'b'}@`);
     console.log(serialPort);
