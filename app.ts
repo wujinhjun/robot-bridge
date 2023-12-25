295093fc840ce5dce8b71e7058fc0dfcfc694129
wujinhjun <wujinhjun@outlook.com>
2023-12-25 23:38:35 +0800

    feat: the sis service

--- app.ts
+++ app.ts
@@ -52,7 +52,7 @@ ioServer.on('connection', (socket) => {
   socket.on('chat audio', (msg) => {
     const data = msg;
 
-    socket.emit('chat audio', data);
+    socket.emit('chat audio', { data, type: 'START' });
 
     fs.writeFileSync(inputPath, data.data);
 
@@ -60,102 +60,190 @@ ioServer.on('connection', (socket) => {
 
     execSync(ffmpegCommand, { stdio: 'inherit' });
 
-    let ts = Number.parseInt(String(new Date().getTime() / 1000));
-
-    const wssUrl =
-      XF_HOST_URL +
-      '?appid=' +
-      XF_APP_ID +
-      '&ts=' +
-      ts +
-      '&signa=' +
-      getSigna(ts);
+    // 华为的接口
+    const result: string[] = [];
 
     const config = {
       file: outputFilePath,
       highWaterMark: 1280,
     };
-
-    const ws = new Websocket(wssUrl);
-    const result: any[] = [];
+    const ws = new Websocket(
+      `wss://${data.endpoint}/v1/${data.projectId}/rasr/continue-stream`,
+      {
+        headers: {
+          'X-Auth-Token': data.token,
+        },
+      },
+    );
 
     ws.on('open', () => {
-      console.log('websocket connect!');
+      const body = {
+        command: 'START',
+        config: {
+          audio_format: 'pcm16k16bit',
+          property: 'chinese_16k_general',
+          add_punc: 'yes',
+        },
+      };
+
+      ws.send(JSON.stringify(body));
     });
 
-    ws.on('message', (data, error) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      const res = JSON.parse(data as unknown as string);
-
-      switch (res.action) {
-        case 'error':
-          console.log(`error code: ${res.code}, error message: ${res.desc}`);
-          break;
-        case 'started':
-          console.log('started');
-          console.log('sid is:' + res.sid);
+    ws.on('message', (msg) => {
+      const data = JSON.parse(msg as unknown as string);
+      //   console.log(msg.toSt);
+      console.log(data);
+      let resultStr = '';
+      switch (data.resp_type) {
+        case 'START':
           const readerStream = fs.createReadStream(config.file, {
             highWaterMark: config.highWaterMark,
           });
 
           readerStream.on('data', (chunk) => {
-            // console.log(chunk.length);
-
             ws.send(chunk);
           });
 
           readerStream.on('end', function () {
             // 最终帧发送结束
-            ws.send('{"end": true}');
+            ws.send('{"command": "END","cancel": false}');
           });
           break;
-        case 'result':
-          const data = JSON.parse(res.data);
-          //   console.log('result is:' + res.result.ws);
-          //   console.log(data.cn.st.rt);
-          console.log(data);
+        case 'RESULT':
+          console.log(data.segments[0].result.text);
+          const text = data.segments[0].result.text;
+          //   resultStr += result.segments[0].result.text;
+          result.push(text);
+          console.log(result);
 
-          //   fs.writeFileSync('./test.txt', '\n');
-          fs.appendFileSync('./test.txt', `\n${res.data}\n`);
-          //   fs.writeFileSync('./test.txt', '\n');
+          break;
+        case 'END':
+          console.log('转写完成');
+          const summary = result.join('');
 
-          result[data.seg_id] = data;
-          if (data.cn.st.type == 0) {
-            result.forEach((item) => {
-              let str = '实时转写';
-
-              str +=
-                item.cn.st.type === 0
-                  ? '【最终】识别结果：'
-                  : '【中间】识别结果：';
-
-              item.cn.st.rt.forEach((rtItem: any) => {
-                rtItem.ws.forEach((wsItem: any) => {
-                  wsItem.cw.forEach((cwItem: any) => {
-                    str += cwItem.w;
-                  });
-                });
-              });
-
-              console.log(str);
+          if (summary.length === 0) {
+            ioServer.emit('chat audio', {
+              data: '没有识别到内容',
+              type: 'ERROR',
             });
+            return;
           }
+          ioServer.emit('chat audio', {
+            data: summary,
+            type: 'RESULT',
+          });
+          break;
+        case 'FATAL_ERROR':
+          console.log('连接中断');
           break;
       }
     });
 
-    ws.on('close', () => {
-      console.log('websocket close!');
+    ws.on('error', (err) => {
+      console.log(err);
     });
 
-    ws.on('error', (err) => {
-      console.log('websocket error!');
-      console.error(err);
-    });
+    // end
+
+    // // 讯飞的用法
+    // let ts = Number.parseInt(String(new Date().getTime() / 1000));
+
+    // const wssUrl =
+    //   XF_HOST_URL +
+    //   '?appid=' +
+    //   XF_APP_ID +
+    //   '&ts=' +
+    //   ts +
+    //   '&signa=' +
+    //   getSigna(ts);
+
+    // const config = {
+    //   file: outputFilePath,
+    //   highWaterMark: 1280,
+    // };
+
+    // const ws = new Websocket(wssUrl);
+    // const result: any[] = [];
+
+    // ws.on('open', () => {
+    //   console.log('websocket connect!');
+    // });
+
+    // ws.on('message', (data, error) => {
+    //   if (error) {
+    //     console.error(error);
+    //     return;
+    //   }
+
+    //   const res = JSON.parse(data as unknown as string);
+
+    //   switch (res.action) {
+    //     case 'error':
+    //       console.log(`error code: ${res.code}, error message: ${res.desc}`);
+    //       break;
+    //     case 'started':
+    //       console.log('started');
+    //       console.log('sid is:' + res.sid);
+    //       const readerStream = fs.createReadStream(config.file, {
+    //         highWaterMark: config.highWaterMark,
+    //       });
+
+    //       readerStream.on('data', (chunk) => {
+    //         // console.log(chunk.length);
+
+    //         ws.send(chunk);
+    //       });
+
+    //       readerStream.on('end', function () {
+    //         // 最终帧发送结束
+    //         ws.send('{"end": true}');
+    //       });
+    //       break;
+    //     case 'result':
+    //       const data = JSON.parse(res.data);
+    //       //   console.log('result is:' + res.result.ws);
+    //       //   console.log(data.cn.st.rt);
+    //       console.log(data);
+
+    //       //   fs.writeFileSync('./test.txt', '\n');
+    //       fs.appendFileSync('./test.txt', `\n${res.data}\n`);
+    //       //   fs.writeFileSync('./test.txt', '\n');
+
+    //       result[data.seg_id] = data;
+    //       if (data.cn.st.type == 0) {
+    //         result.forEach((item) => {
+    //           let str = '实时转写';
+
+    //           str +=
+    //             item.cn.st.type === 0
+    //               ? '【最终】识别结果：'
+    //               : '【中间】识别结果：';
+
+    //           item.cn.st.rt.forEach((rtItem: any) => {
+    //             rtItem.ws.forEach((wsItem: any) => {
+    //               wsItem.cw.forEach((cwItem: any) => {
+    //                 str += cwItem.w;
+    //               });
+    //             });
+    //           });
+
+    //           console.log(str);
+    //         });
+    //       }
+    //       break;
+    //   }
+    // });
+
+    // ws.on('close', () => {
+    //   console.log('websocket close!');
+    // });
+
+    // ws.on('error', (err) => {
+    //   console.log('websocket error!');
+    //   console.error(err);
+    // });
+
+    // // 结束
   });
 });
 
